fcf9703e4a00039bd26ab0f9c1238f63f543a3e8
LaiSha Buck <laishabuck@gmail.com>
2023-05-14 17:04:32 -0400

    employee table created

--- db/schema.sql
+++ db/schema.sql
@@ -7,3 +7,11 @@ CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   department_name VARCHAR(30) NOT NULL
 );
+
+CREATE table employees (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  manager_id INT NULL,
+  role_id INT
+);
