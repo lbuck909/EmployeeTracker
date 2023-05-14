20d6e52f4793135bb2ba3dbcea62e681d1f65a7d
LaiSha Buck <laishabuck@gmail.com>
2023-05-14 19:48:33 -0400

    env info added

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const connection = mysql.createConnection(
   {
     host: 'localhost',
     user: 'root',
-    password: ''
+    password: 'spicy$%^wolf'
     // database: 'employee_db'
   },
   // console.log(`Connected to the employee_db database.`)
@@ -30,3 +30,18 @@ const connection = mysql.createConnection(
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
   });
+
+  require('dotenv').config()
+  console.log(`Database name is ${process.env.DB_NAME}`);
+  console.log(`Database username is ${process.env.DB_USERNAME}`);
+  console.log(`Database password is ${process.env.DB_PASSWORD}`);
+
+// prompt functions
+function newPrompt(){
+  inquirer.prompt({
+    name: "menu",
+    type: "list",
+    message: "What data would you like view?",
+    choices: ['View All Roles', 'View All Departments', 'View All Emplyees', ' Add a Department', ' Add a Role', 'Add a Employee', 'Update Employee Role' ],
+  })
+}
