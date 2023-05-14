9c1b8119489ca365ad850116bf0cc2f8fe86a57b
LaiSha Buck <laishabuck@gmail.com>
2023-05-14 16:48:15 -0400

    error request response added

--- server.js
+++ server.js
@@ -5,3 +5,10 @@ const mysql = require('mysql');
 const PORT = process.env. PORT || 3001;
 const app = express();
 // Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default repsonse for an error request
+app.use((req, res) => {
+  res.status(404).end();
+})
